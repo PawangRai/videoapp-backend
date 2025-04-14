7fa40d65ddc945996610cfb23a572b594d6d0a5a
Pawang Rai <94522697+PawangRai@users.noreply.github.com>
2025-04-14 20:02:30 +0500

    configured ApiResponse and ApiError and also made an asyncHandler utility

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"})) // for limiting json data
+
+app.use(express.urlencoded({extended: true, limit: "16kb"})) // for accessing data through urls
+
+app.use(express.static("public")) // for storing files, etc
+
+export { app }
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,28 @@
+// asyncHanlder is a higher order function. A higher order function either takes in a function as a parameter or returns a function. Here, it is taking in a function.
+
+// It is basically a wrapper that takes in a function
+
+const asyncHandler = (requestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((error) => next(error))
+    }
+}
+
+export { asyncHandler }
+
+// const asycnHandler = () => {}
+// const asycnHandler = (func) => () => {}
+// const asycnHandler = (func) => async () => {}
+
+
+
+// const asycnHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
