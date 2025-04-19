198aab93e8c35e6152781dafc293a9f95d082123
Pawang Rai <94522697+PawangRai@users.noreply.github.com>
2025-04-19 16:03:30 +0500

    fixed and tested user.controller

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -17,7 +17,9 @@ const uploadOnCloudinary = async (localFilePath) => {
         })
 
         // file has been uploaded successfully
-        console.log("File has been uploaded on cloudinary", response.url);
+        // console.log("File has been uploaded on cloudinary", response.url);
+        fs.unlinkSync(localFilePath)
+        // console.log("This is the cloudinary response ", response);
         return response;
 
     } catch (error) {
