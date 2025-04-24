31265c14891df46d992ecf18c94ed25141fc9a77
Pawang Rai <94522697+PawangRai@users.noreply.github.com>
2025-04-24 20:27:14 +0500

    feat: add routes and controllers for comments, likes, playlists, subscriptions, tweets, and videos with JWT verification

--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -1,2 +1,17 @@
 import { Router } from "express";
-import {}
+import {
+  addComment,
+  deleteComment,
+  getVideoComments,
+  updateComment,
+} from "../controllers/comment.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+router.use(verifyJWT); // Apply verifyJWT middleware to add routes in this file
+
+router.use("/:videoId").get(getVideoComments).post(addComment)
+router.use("/c/:commentId").delete(deleteComment).patch(updateComment)
+
+export default router
